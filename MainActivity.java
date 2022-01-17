ab4914604f31ebfb5869996a2604df8ab54971a6
Agiliis <97856537+Agiliis@users.noreply.github.com>
2022-01-17 15:48:43 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.eyetrackingapk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static String TAG = "OpenCVinit";
+    File cascFile;
+    CascadeClassifier faceDetector;
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mRgba;
+
+   /* static {
+        if (!OpenCVLoader.initDebug()) Log.d(TAG, "OpenCV not loaded");
+        else Log.d(TAG, "OpenCV loaded");
+    } */
+
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
+        @Override
+        public void onManagerConnected(int status){
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    try {
+                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        cascFile = new File(cascadeDir, "haarcascade_frontalface_alt.xml");
+
+                        FileOutputStream fos = new FileOutputStream(cascFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, bytesRead);
+                        }
+
+                        is.close();
+                        fos.close();
+
+                        faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+
+                        if (faceDetector.empty())
+                            faceDetector = null;
+                        else
+                            cascadeDir.delete();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+
+                    cameraBridgeViewBase.setCameraIndex(1);
+                    cameraBridgeViewBase.enableView();
+                    break;
+                default:
+                    try {
+                        super.onManagerConnected(status);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},1);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.cameraSuperficie);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(MainActivity.this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    cameraBridgeViewBase.setCameraPermissionGranted();
+                }else{
+                    //permissão negada
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()){
+            Log.d(TAG,"OpenCV not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,this,baseLoaderCallback);
+        }else{
+            try {
+                baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (cameraBridgeViewBase != null) cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (cameraBridgeViewBase != null) cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+        //mRgbat = new Mat();
+        //dst = new Mat();
+        //mGray = new Mat();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        //Core.transpose(mRgba, mRgbat);
+        Core.flip(mRgba, mRgba, 1);
+
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(mRgba,faceDetections);
+
+        for(Rect rect : faceDetections.toArray()){
+            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255,0,0));
+        }
+        /*Imgproc.resize(mRgbat, dst, mRgba.size());
+        mRgba.release();
+        //mRgbat.release();*/
+        return mRgba;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        //mRgbat.release();
+        //dst.release();
+        //mGray.release();
+    }
+}
